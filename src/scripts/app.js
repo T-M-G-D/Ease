1d4681459a5a5dc9f1a8975530077b7b7e6bf30f
Thomas Marique <thomas_marique@hotmail.com>
2022-05-23 22:16:23 +0200

    index finished : added darkmode, nav, footer and style

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,4 @@
+"use strict"
 // Date copyright
 
 var date = new Date();
@@ -5,3 +6,41 @@ var date = date.getFullYear();
 var output = document.querySelector(".footer__output")
 
 output.innerHTML = date;
+
+
+// Darkmode
+// Source : Philippe Thronte, professeur dans l'option DWT à la HEAJ
+
+const darkTheme = document.querySelector(".darkMode");
+const lightTheme = document.querySelector(".lightMode");
+
+//Gérer le data-theme du body
+darkTheme && lightTheme.addEventListener("click", function() {
+    if (document.body.dataset.theme === "dark") {
+        light();
+        localStorage.setItem("theme", "light");
+    } else {
+        dark();
+        localStorage.setItem("theme", "dark");
+    }
+});
+
+//Est ce que l'utilisateur veut un theme dark?
+const userDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+//Est ce que l'utilisateur a déjà indiqué une préférence sur notre site?
+let theme = localStorage.getItem('theme');
+if ((!theme && userDark) || (theme === "dark")) {
+    dark();
+} else if (theme === "light") {
+    light();
+}
+
+//function pour le dark
+function dark() {
+    document.body.setAttribute("data-theme", "dark");
+}
+//function pour le light
+function light() {
+    document.body.setAttribute("data-theme", "light");
+}
